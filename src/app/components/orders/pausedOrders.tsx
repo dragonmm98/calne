485a5054662cda87ab75a880d4172ddf74189691
dragonmm98 <3d.dragonmm@98gmail.com>
2023-11-24 23:33:27 +0900

    feat: almost finished orders page 2023.24.11

---
+++ src/app/components/orders/pausedOrders.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { Box, Button, Stack } from "@mui/material";
+import { TabPanel } from "@mui/lab";
+import moment from "moment";
+import "../../../css/order.css";
+
+const pausedOrders = [
+    [1,2,3]
+    
+];
+
+export default function PausedOrders (props:any) {
+    return (
+        <TabPanel value="1">
+            <Stack>
+                {pausedOrders?.map((order) => {
+                    return (
+                        <Box className={"order_main_box"}>
+                      <Box className={"order_box_scroll"}>
+                        {order.map((item) => {
+                            const image_path = `/others/foodmm.jpg`;
+                            return (
+                                <Box className={"ordersName_price"}>
+                                    <img src={image_path} className={"orderDishImg"} alt=""/>
+                                    <p className="titleDish">Qovurilgan go’sht cho’poncha</p>
+                                    <Box className={"priceBox"}>
+                                        <p>$7</p>
+                                        <img src="/icons/Close.svg" alt="" />
+                                        <p>$3</p>
+                                        <img src="/icons/pause.svg" alt=""/>
+                                        <p style={{marginLeft: "4px"}}>$21</p>
+                                    </Box>
+                                     </Box>
+                            );
+                        })}
+                      </Box>
+
+                               <Box className={"total_price_box black_solid"}>
+                                <Box className={"boxTotal"}>
+                                    <p>mahsulot narxi </p>
+                                    <p>$22</p>
+                                    <img src="/icons/plus.svg" alt="" style={{marginLeft: "20px"}}/>
+                                    <p>yetkazish xizmati </p>
+                                    <p>$2 </p>
+                                    <img src="/icons/pause.svg" alt="" 
+                                    style={{ marginLeft: "20px"}}/>
+                                    <p>Jami narx </p>
+                                    <p>$110</p>
+                                </Box>
+                           
+                                  <Button className="red_button_box" variant="contained" >bekor qilish</Button>
+                                
+                              
+                                  <Button className="blue_button_box" variant="contained"
+                                   > To'lash</Button>
+                                 
+
+                               </Box>
+                        </Box>
+                    );
+                })}
+            </Stack>
+        </TabPanel>
+    );
+}
--- src/app/screens/OrdersPage/index.tsx
+++ src/app/screens/OrdersPage/index.tsx
@@ -1,8 +1,92 @@
-import { Container } from "@mui/system";
-import React from "react";
+import "../../../css/order.css"
+import React, { useState } from "react";
+import { Box, Container, Stack } from "@mui/material";
+import Tab from '@mui/material/Tab';
+import { TabContext, TabList } from "@mui/lab";
+import PausedOrders from "../../components/orders/pausedOrders";
+import Marginer from "../../components/marginer";
+import ProcessOrders from "../../components/orders/proccessOrders";
+
+
 
 export function OrdersPage(){
+    /*** INITITIALIZATIONS***/
+    const [value, setValue] = useState("1");
+   
+     /**HANDLERS **/
+     const handleChange= (event: any, newValue: string) => {
+        setValue(newValue);
+     }
+
     return(
-        <Container>OrdersPage</Container>
+        <div className="order_page">
+        <Container
+        maxWidth="lg"
+        style={{display: "flex", flexDirection:"row"}}
+        sx={{ mt: "50px", mb: "50px"}}>
+           
+           <Stack className="order_left">
+            <TabContext value={value}>
+                <Box className="order_nav_frame">
+                    <Box sx={{borderBottom: 1, borderColor: "white"}}>
+                        <TabList
+                        className="tablist_label"
+                        onChange={handleChange}
+                        // value={value}
+                        aria-label="basic tabs example"
+                        style={{display: "flex", justifyContent: "space-between",}}>
+                           <Tab  label="Buyurtmalarim" value={1}/>                 
+                           <Tab  label="Jarayon" value={2}/>                 
+                           <Tab  label="Yakunlangan" value={3}/>                
+                        </TabList>
+                    </Box>
+                </Box>
+                <Stack className="order_main_content">
+                      <PausedOrders/>
+                      <ProcessOrders/>
+                </Stack>
+                 </TabContext>
+           </Stack>
+
+           <Stack className="order_right">
+            <Box className={"order_info_box"}>
+                <Box 
+                display={"flex"}
+                flexDirection={"column"}
+                alignItems={"center"}>
+                    <div className="order_user_img">
+                        <img src="/icons/default_user.svg" alt=""
+                        className="order_user_avatar"/>
+                        <img className="avatar_1" src="/icons/user1.svg" alt=""/>
+                    </div>
+                    <span className="Ismoilov-Akmaljon">
+                          Ismoilov Akmaljon
+                               </span>
+                               <span className="Foydalanuvchi">
+                                    Foydalanuvchi
+                             </span> 
+                             <Box className={"marginer_botline"}>
+                            <Marginer
+                             direction="horizontal"
+                             height="2"
+                             width="332"
+                            bg="#A1A1A1;"/>
+                            
+                            <Box className={"location_bottom"}>
+                                <img src="/icons/location1.svg"/>
+                                <span className="location">
+                                    Seoul
+                                </span>
+                            </Box>
+
+
+                    </Box>
+                </Box>
+            </Box>
+           </Stack>
+
+        </Container>
+
+        </div>
     )
 }
