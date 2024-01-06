55a0d0011c4c2c305f6a0c912d8bda8bf61dcad7
dragonmm98 <3d.dragonmm@98gmail.com>
2024-01-06 17:17:16 +0900

    feat: develop MemberPage query url

--- src/app/screens/MemberPage/index.tsx
+++ src/app/screens/MemberPage/index.tsx
@@ -1,18 +1,29 @@
 import React from "react";
-import { Route, Switch, useRouteMatch } from "react-router-dom";
+import { Route, Switch, useRouteMatch,useLocation } from "react-router-dom";
 import { VisitMyPage } from "./VisitMyPage";
 import { VisitOtherPage } from "./VisitOtherPage";
 import "../../../css/my_page.css";
 
+function useQuery () {
+    const {search} = useLocation();
+    return React.useMemo(() => new URLSearchParams(search), [search]);
+} 
+
 export function MembersPage(props: any){
     const {verifiedMemberData} = props;
     let member = useRouteMatch();
+    const query = useQuery();
+    const chosen_mb_id: string | null = query.get("mb_id") ?? null;
+    const chosen_art_id: string | null = query.get("art_id") ?? null;
   
     return(
         <div className="member_page">
             <Switch>
                 <Route path={`${member.path}/other`}>
-                    <VisitOtherPage verifiedMemberData={verifiedMemberData}/>
+                    <VisitOtherPage 
+                    verifiedMemberData={verifiedMemberData}
+                    chosen_mb_id={chosen_mb_id}
+                    chosen_art_id={chosen_art_id}/>
                 </Route>
                 
                 <Route path={`${member.path}`}>
