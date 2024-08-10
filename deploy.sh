fd638cc300b81276358076127b2d7ced16f8fc05
dragonmm98 <3d.dragonmm@98gmail.com>
2024-08-11 06:38:37 +0900

    feat: deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@
 #PRODUCTION 
 git reset --hard
 git pull origin master
+
 npm i yarn -g 
 yarn
 yarn run build 
-
-pm2 start "yarn run start:prod"
+pm2 start "yarn run start:prod" --name=Calne-react
 
