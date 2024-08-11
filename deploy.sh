a38555df91a604a0ac8c84f1f5ff7aa4fff0fad9
dragonmm98 <3d.dragonmm@98gmail.com>
2024-08-12 04:29:13 +0900

    fix: deploy.sh and .env

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,8 @@ git reset --hard
 git pull origin master
 
 npm i yarn -g 
+yarn global add serve
 yarn
 yarn run build 
-pm2 start "yarn run start:prod" --name=Calne-react
+pm2 start serve-s build --name=Calne-react
 
